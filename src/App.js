b9a26b6fe3eb65207f158083b0d511002d0d4118
Antonio Codella <codellaantonio1992@gmail.com>
2022-05-16 10:52:30 +0200

    jsx-01

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+const hello = <h1>Hello, World!</h1>;
 function App() {
   return (
     <div className="App">
